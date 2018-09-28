97f332c24ff5fbb392ff5b046e6cf57565c34746
Sergey Bizin <sbizin@emdev.ru>
2018-09-28 17:28:26 +0700

    Add s2i

--- standalone/liferay_wildfly_bundler.sh
+++ standalone/liferay_wildfly_bundler.sh
@@ -2,19 +2,6 @@
 
 export SCRIPT_DIR="$( cd "$( dirname "$0" )" > /dev/null && pwd )"
 
-sh "$SCRIPT_DIR/bin/ljb-setup-start.sh" true
-sh "$SCRIPT_DIR/bin/ljb-setup-appserver.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-jdbc-driver.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-mail-store.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-liferay.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-liferay-cluster.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-liferay-deployment.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-prometheus.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-finish.sh"
+sh "$SCRIPT_DIR/build.sh"
 
-sh "$SCRIPT_DIR/bin/ljb-setup-start.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-jdbc-connection.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-mail-cred.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-jks.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-elastic.sh"
-sh "$SCRIPT_DIR/bin/ljb-setup-finish.sh"
+sh "$SCRIPT_DIR/setup.sh"
