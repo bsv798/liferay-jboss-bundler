1d5c7b9d6ee597eb738319e4716d9233a1b8fa1c
Sergey Bizin <bsv798@gmail.com>
2018-10-01 21:47:23 +0700

    Add run.sh script

---
+++ standalone/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export SCRIPT_DIR="$( cd "$( dirname "$0" )" > /dev/null && pwd )"
+
+. "$SCRIPT_DIR/bin/ljb-setup-conf.sh"
+
+"$APPSERVER_HOME_PATH/bin/standalone.sh" "-b" "0.0.0.0" "$@"
