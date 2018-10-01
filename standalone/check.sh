6f8f2e47b875106387c03eb903cab2bf23697953
Sergey Bizin <bsv798@gmail.com>
2018-10-01 22:10:53 +0700

    Add check.sh

---
+++ standalone/check.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export SCRIPT_DIR="$( cd "$( dirname "$0" )" > /dev/null && pwd )"
+
+. "$SCRIPT_DIR/bin/ljb-setup-conf.sh"
+
+if [ "x$JKS_FILE_PATH" != "x" ] && [ "x$JKS_STORE_PASSWORD" != "x" ] && [ "x$JKS_KEY_PASSWORD" != "x" ]
+then
+	curl -f -s -k -o /dev/null https://0.0.0.0:8443/ || exit 1
+else
+	curl -f -s -o /dev/null http://0.0.0.0:8080/ || exit 1
+fi
